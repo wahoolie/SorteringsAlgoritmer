d74e924b6629c28f4d1dc3b8bd56d3ce3e2f0c39
wahoolie <89255527+wahoolie@users.noreply.github.com>
2022-02-18 12:57:27 +0100

    Add files via upload

---
+++ C# Uppgift/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace C__Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        var watch = new System.Diagnostics.Stopwatch();
+        watch.Start();
+
+        int[] arr = new int[1000];
+        int n = 1000;
+        Console.WriteLine("Selection sort");
+        Console.Write("Initial array is: ");
+        for (int i = 0; i < n; i++) {
+        Console.Write(arr[i] + " ");
+        }
+        int temp, smallest;
+        for (int i = 0; i < n - 1; i++) {
+         smallest = i;
+         for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[smallest]) {
+               smallest = j;
+            }
+         }
+         temp = arr[smallest];
+         arr[smallest] = arr[i];
+         arr[i] = temp;
+      }
+        Console.WriteLine();
+        Console.Write("Sorted array is: ");
+        for (int i = 0; i < n; i++) {
+        Console.Write(arr[i] + " ");
+        watch.Stop();
+        Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+      }
+   }
+ }
+}
